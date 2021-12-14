6eb7c4846e0b43bd2c3cb94e9ac1c63ecd454f50
superRaptor911 <raptor.inc2018@gmail.com>
2021-12-14 13:17:02 +0530

    moved to config folder

--- dotfiles.sh
+++ dotfiles.sh
@@ -2,13 +2,6 @@
 
 base_dir=".config"
 files=(
-        "$base_dir/i3/"
-        "$base_dir/nvim/"
-        "$base_dir/alacritty/"
-        "$base_dir/fish/"
-        "$base_dir/lf/"
-        "$base_dir/nvim/"
-        "$base_dir/polybar/"
         ".Xresources"
         ".bashrc"
         ".vimrc"
@@ -17,4 +10,23 @@ files=(
 for i in "${files[@]}"
 do
     cp -rva ~/$i ./
+done
+
+
+files=(
+        "$base_dir/i3/"
+        "$base_dir/nvim/"
+        "$base_dir/alacritty/"
+        "$base_dir/fish/"
+        "$base_dir/lf/"
+        "$base_dir/nvim/"
+        "$base_dir/polybar/"
+    )
+
+
+mkdir -p ./.config/
+
+for i in "${files[@]}"
+do
+    cp -rva ~/$i ./config/
 done
